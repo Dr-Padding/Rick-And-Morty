0431bce07411fdbe84f36f22dc0082a7325986f3
Dr-Padding <dr.padding@gmail.com>
2021-09-23 22:00:37 +0100

    Initial commit

---
+++ app/src/main/java/com/drawing/rickandmorty/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.drawing.rickandmorty.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.drawing.rickandmorty.R
+import com.drawing.rickandmorty.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.schedule
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_RickAndMorty)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
